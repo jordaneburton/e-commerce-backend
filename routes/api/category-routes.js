67afea591ff32282f5af4c905d8941915c1baba9
Jordan <jordane.burton@gmail.com>
2024-02-07 18:43:39 -0600

    finished put and post routes for tag and category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,11 +50,6 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
-    const catData = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    })
     const checkData = await Category.findOne({
       where:{id: req.params.id}
     })
@@ -62,6 +57,12 @@ router.put('/:id', async (req, res) => {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
+    
+    const catData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
 
     return res.status(200).json(catData);
   } catch (err) {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,12 +35,37 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const checkData = await Tag.findOne({
+      where:{id: req.params.id}
+    })
+    if (!checkData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    return res.status(200).json(tagData);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
